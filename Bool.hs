3e8c4130c7a6634cf82d16c46fe586a5a8ae9d9c
Vitor Emanuel Rodrigues de Alencar <vitor.alencar.713@ufrn.edu.br>
2023-11-11 19:53:11 -0300

    Criando mais funções e organizando o arquivo

--- Bool.hs
+++ Bool.hs
@@ -1,25 +1,36 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Use camelCase" #-}
 module Bool where
+    
+import Prelude hiding 
+    (Bool, True, False, not, (&&), (||), xou, if_then_else)
+import Nat
 
-import Prelude hiding (True, False, Bool, (&&), (||), xou, if_then_else)
+data Bool = False | True
+    deriving ( Eq , Show )
 
-data Bool = True | False
-    deriving (Eq, Show)
---conjunção
+-- Negação
+not :: Bool -> Bool
+not True = False
+not _ = True
+
+-- Conjunção
 (&&) :: Bool -> Bool -> Bool
 True && True = True
 _ && _ = False
---disjunção
+
+-- Disjunção
 (||) :: Bool -> Bool -> Bool
 False || False = False
 _ || _ = True
---disjunção exclusiva
+
+-- Disjunção Exclusiva
 xou :: Bool -> Bool -> Bool
 True `xou` True = False
 False `xou` False = False
 _ `xou` _ = True
---se_então-nat
-if_then_else :: Bool -> a -> a -> a
+
+-- Se_bla, então bli
+if_then_else :: Bool -> Nat -> Nat -> Nat
 if_then_else True n _ = n
-if_then_else False _ m = m
+if_then_else False _ n = n
--- Nat.hs
+++ Nat.hs
@@ -1,63 +1,119 @@
 module Nat where
-import Prelude 
-  hiding ((+), (*), (^), (-), double, pred, fact, fib, quot, min, max, gcd, lcm, div, rem)
+
+import Prelude hiding 
+    (O, S, (+), (*), (^), (-), double, pred, fact, fib, quot, min, max, monus, gcd, lcm, div, rem, if_then_else)
 
 data Nat = O | S Nat
-  deriving (Eq , Show)
+    deriving (Eq , Show)
 
--- soma
+-- + (Soma)
 (+) :: Nat -> Nat -> Nat
 m + O = m
 m + (S n) = S (m + n)
--- multiplicação
+
+-- * (Multiplicação)
 (*) :: Nat -> Nat -> Nat
 _ * O = O
 m * (S n) = m + (m * n)
--- exponencial
+
+-- ^ (Exponencial)
 (^) :: Nat -> Nat -> Nat
 _ ^ O = S O
 m ^ S O = m
 m ^ (S n) = m * (m ^ n)
--- subtraçao
+
+-- (-) Subtração
 (-) :: Nat -> Nat -> Nat
 O - n = O
 m - O = m
 (S m) - (S n) = m - n
--- dobro
+
+-- double (Dobro)
 double :: Nat -> Nat
 double O = O
 double (S m) = S (S (double m))
--- predecessor
+
+-- pred (Predecessor)
 pred :: Nat -> Nat
 pred O = O
 pred (S m) = m
--- factorial
+
+-- fact (Fatorial)
 fact :: Nat -> Nat
 fact O = S O
 fact (S m) = S m * fact m
--- fibonacci
+
+-- fib (Fibonacci)
 fib :: Nat -> Nat
 fib O = O
 fib (S O) = S O
 fib (S (S n)) = fib (S n) + fib n
--- mínimo
+
+-- min (Mínimo)
 min :: Nat -> Nat -> Nat
 min (S m) (S n) = S (min m n)
 min O _ = O
 min _ O = O
--- máximo
+
+-- max (Máximo)
 max :: Nat -> Nat -> Nat
 max O m = m
 max m O = m
 max (S m) (S n) = S (max m n)
--- div - Thanos definiou durante a aula
---div :: (Nat, Nat) -> (Nat, Nat)
---div (m, n)
-  -- m < n     = (O, n)
-  --otherwise = let (q', r') = div (m - n, n)
-                -- in (S q', r')
--- quot
--- rem
--- gcd
--- lcm
 
+-- monus (Uma Subtração Diferente)
+monus :: Nat -> Nat -> Nat
+monus m O = m
+monus m (S n) = pred (monus m n)
+
+-- div (Divisão) 
+div :: Nat -> Nat -> (Nat, Nat)
+div _ O = error "Zero is not divisor!"
+div n m = 
+    if leq n m then (O, n) 
+    else (S n', m')
+        where (n', m') = div (monus n m) m
+
+-- quot (Quociente)
+quot :: Nat -> Nat -> Nat
+quot n m = fst(div n m)
+
+-- rem (Resto da Divisão)
+rem :: Nat -> Nat -> Nat
+rem n m = snd(div n m)
+
+-- gcd (???)
+-- lcm (???)
+
+-- leq (Menor que)
+leq :: Nat -> Nat -> Bool
+leq O _ = True
+leq _ O = False
+leq (S m) (S n) = leq m n
+
+-- geq (Maior que)
+geq :: Nat -> Nat -> Bool
+geq O _ = False
+geq _ O = True
+geq (S m) (S n) = geq m n
+
+-- ev (Par)
+ev :: Nat -> Bool
+ev O = True
+ev (S m) = od m
+
+-- od (Ímpar)
+od :: Nat -> Bool
+od O = False
+od (S m) = ev m
+
+-- isMul3 (Múltiplo de 3)
+isMul3 :: Nat -> Bool
+isMul3 O = True
+isMul3 (S(S(S m))) = isMul3 m
+isMul3 _ = False
+
+-- isZero (É zero sksk)
+isZero :: Nat -> Bool
+isZero O = True
+isZero _ = False
