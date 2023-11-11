a1b92f85c4fd91ee22e4b15e4b0237b3a10eb5c5
Vitor Emanuel Rodrigues de Alencar <vitor.alencar.713@ufrn.edu.br>
2023-11-11 17:09:06 -0300

    Finalizando Type Bool

--- Bool.hs
+++ Bool.hs
@@ -1,7 +1,25 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use camelCase" #-}
 module Bool where
 
-import Prelude hiding (True, False, Bool)
+import Prelude hiding (True, False, Bool, (&&), (||), xou, if_then_else)
 
 data Bool = True | False
     deriving (Eq, Show)
-
+--conjunção
+(&&) :: Bool -> Bool -> Bool
+True && True = True
+_ && _ = False
+--disjunção
+(||) :: Bool -> Bool -> Bool
+False || False = False
+_ || _ = True
+--disjunção exclusiva
+xou :: Bool -> Bool -> Bool
+True `xou` True = False
+False `xou` False = False
+_ `xou` _ = True
+--se_então-nat
+if_then_else :: Bool -> a -> a -> a
+if_then_else True n _ = n
+if_then_else False _ m = m
